d7db223baa652d27f484901b60f80e565cdd07a1
Siya-stackit <61430304+Siya-stackit@users.noreply.github.com>
2021-02-01 11:38:15 +0200

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,1 +1,25 @@
-alert("I am working")
+function convert(amount, rate){
+
+    //conversion and return answer.
+    var convertedAnswer = amount* rate;
+    return convertedAnswer;
+}
+
+//main
+function main(){
+    //exchange rates constants will go here
+
+
+    var amount = document.getElementById("first").value; // zar
+    
+    var rate = 16; //dollar
+    var ans = convert(amount, rate);
+    if(!isNaN(amount) && amount !=0){
+       // document.getElementById("second").innerHTML = 5+6;
+        alert(convert(amount, rate)); // need to change so i display in the htm file
+    }
+    else{
+        alert("Please enter an Interger value...");
+    }
+    
+}
